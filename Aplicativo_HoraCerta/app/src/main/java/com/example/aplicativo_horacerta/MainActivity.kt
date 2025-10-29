bc758fab6f2d470a031ee463107c294ce5cc12f2
Filipe Ribeiro Simões <filipe.rs@puccampinas.edu.br>
2025-10-29 01:47:14 -0300

    merge?

--- Aplicativo_HoraCerta/app/src/main/java/com/example/aplicativo_horacerta/MainActivity.kt
+++ Aplicativo_HoraCerta/app/src/main/java/com/example/aplicativo_horacerta/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.example.aplicativo_horacerta
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        //setContentView(R.layout.ok_inicio1_login_ou_registro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 1. Verifica se os Termos foram aceitos
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        val onboardingDone = prefs.getBoolean("onboarding_done", false)
+
+        if (!onboardingDone) {
+            // Se NÃO aceito: Vai para os Termos
+            startActivity(Intent(this, TermosIniciaisActivity::class.java))
+            finish()
+        } else {
+            // Se JÁ aceito: Vai para a tela inicial de Login/Registro
+            startActivity(Intent(this, InicioTelaActivity::class.java)) // <-- AQUI USAMOS A NOVA TELA DE INÍCIO!
+            finish()
         }
     }
 }
+
 
 //aaa123
